303625fc8fd49539cfdd58cd8c0f7c45725890a3
Ajay Baghel <87561839+Ajay789896@users.noreply.github.com>
2024-02-27 00:19:11 +0530

    Update ab_position_check.py

--- String Challenges/ab_position_check.py
+++ String Challenges/ab_position_check.py
@@ -1,1 +1,39 @@
+"""
+ * Problem Statement                                            *
+ * Have the function ABCheck(str) take the str parameter being  *
+ * passed and return the string true if the characters a and b  *
+ * are separated by exactly 3 places anywhere in the string at  *
+ * least once (ie. "lane borrowed" would result in true because *
+ * there is exactly three characters between a and b).          *
+ * Otherwise return the string false.
+"""
 
+#First Solution - 
+def abcheck(strn):
+    a = strn.lower()
+    indexofa = []
+    indexofb = []
+    for i in range(len(a)):
+        if a[i] == 'a':
+            indexofa.append(i)
+        elif a[i] == 'b':
+            indexofb.append(i)
+    for i in indexofa:
+        for j in indexofb:
+            if j - i == 4:
+                return True
+    return False
+
+print(abcheck("lane borrowed"))  # Output: True
+print(abcheck("bdjsade"))          # Output: False
+
+#Second Solution - 
+def ABCheck(strn):
+    str_lower = strn.lower()
+    for i in range(len(str_lower) - 4):  # Iterate until the fourth-to-last character
+        if str_lower[i] == 'a' and str_lower[i + 4] == 'b':
+            return "true"
+    return "false"
+
+print(ABCheck("lane borrowed"))  # Output: true
+print(ABCheck("abcde"))          # Output: false
