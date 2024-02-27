20b9278cd3a94927bbff5d371709daa073f03479
Ajay Baghel <87561839+ajay-1110@users.noreply.github.com>
2024-02-27 16:40:54 +0530

    Create binary_conversion.py

---
+++ Maths fundamental/binary_conversion.py
@@ -0,0 +1,21 @@
+"""/****************************************************************
+ *              CODERBYTE BINARY CONVERTER CHALLENGE            *
+ *                                                              *
+ * Problem Statement                                            *
+ * Have the function BinaryConverter(str) return the decimal    *
+ * form of the binary value. For example: if 101 is passed      *
+ * return 5, or if 1000 is passed return 8.                     *
+ *                                                              *
+ *                                                              *
+ * Examples                                                     *
+ * Input 1: "100101"                                            *
+ * Output 1: 37                                                 *
+ *                                                              *
+ * Input 2: "011"                                               *
+ * Output 2: 3                                                  *
+ *                                                              *
+ ***************************************************************/"""
+def binaryconverter(strn):
+    x = int(strn,2)
+    return x
+print(binaryconverter("100101"))
