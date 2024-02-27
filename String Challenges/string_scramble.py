06a3eacdefa23a2fa7945fed03e698316c245bbf
Ajay Baghel <87561839+ajay-1110@users.noreply.github.com>
2024-02-27 15:54:59 +0530

    Create string_scramble.py

---
+++ String Challenges/string_scramble.py
@@ -0,0 +1,30 @@
+"""/****************************************************************
+ *             CODERBYTE STRING SCRAMBLE CHALLENGE              *
+ *                                                              *
+ * Problem Statement                                            *
+ * Have the function StringScramble(str1,str2) take both        * 
+ * parameters being passed and return the string true if a      *
+ * portion of str1 characters can be rearranged to match str2,  *
+ * otherwise return the string false.                           *
+ * For example: if str1 is "rkqodlw" and str2 is "world" the    *
+ * output should return true. Punctuation and symbols will not  *
+ * be entered with the parameters.                              *
+ *                                                              *
+ * Examples                                                     *
+ * Input 1: "cdore" & "coder"                                   *
+ * Output 1: true                                               *
+ *                                                              *
+ * Input 2: "h3llko" & "hello"                                  *
+ * Output 2: false                                              *"""
+
+def stringscramble(str1,str2):
+    raw_str = str1
+    match_str = str2
+    for i in match_str:
+        if i not in raw_str:
+            return False
+        updated_str = raw_str.replace(i,'',1)
+        updated_str = raw_str
+    return True
+
+print(stringscramble("h3llko" , "hello"))
